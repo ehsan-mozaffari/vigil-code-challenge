96cc5b9a0340ec78e5d2aba6f65db39f6afdfc19
Ehsan <ehsan.mozaffari@yahoo.com>
2023-03-04 01:41:47 +0330

    fix issue of the sql queries

--- src/main/resources/db/V1.0__create_user_post_notification_tables.sql
+++ src/main/resources/db/V1.0__create_user_post_notification_tables.sql
@@ -1,16 +1,11 @@
 -- @@@@@@@@@@@ USER TABLE @@@@@@@@@@@
-CREATE TABLE user (
+CREATE TABLE "user" (
 	id serial PRIMARY KEY,
 	name varchar NOT NULL,
 	email varchar UNIQUE NOT NULL,
 	is_online boolean NOT NULL,
-	followed_user_ids integer[] NOT NULL,
-
+	followed_user_ids integer[] NOT NULL
 );
-COMMENT ON COLUMN user.id IS 'user id auto increment';
-COMMENT ON COLUMN user.name IS 'username ';
-COMMENT ON COLUMN user.is_online IS 'if the user is online the value is true else false';
-COMMENT ON COLUMN user.followed_user_ids IS 'Array of userIds that the user is following';
 
 -- @@@@@@@@@@@ POST TABLE @@@@@@@@@@@
 CREATE TABLE post (
@@ -18,8 +13,9 @@ CREATE TABLE post (
 	author_id integer NOT NULL,
 	content varchar NOT NULL,
 	image varchar ,
-	publish_date timestamp without time zone NOT NULL,
+	publish_date timestamp without time zone NOT NULL
 );
+
 -- @@@@@@@@@@@ NOTIFICATION TABLE @@@@@@@@@@@
 CREATE TABLE notification (
 	id serial PRIMARY KEY,
