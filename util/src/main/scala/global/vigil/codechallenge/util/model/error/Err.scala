6e90daaeff70420ada64ca3557239264c4146639
Ehsan <ehsan.mozaffari@yahoo.com>
2023-03-06 23:43:15 +0330

    add ZioJsonCodec Err case class

--- util/src/main/scala/global/vigil/codechallenge/util/model/error/Err.scala
+++ util/src/main/scala/global/vigil/codechallenge/util/model/error/Err.scala
@@ -1,9 +1,12 @@
 package global.vigil.codechallenge.util.model.error
 
-import zio.json.JsonCodec
+import global.vigil.codechallenge.util.json.ZioJsonCodec
+import sttp.tapir.generic.auto.*
+import sttp.tapir.json.zio.*
+import zio.json.*
 
 sealed abstract class Err(m: String)
-object Err{
-  given codec: JsonCodec[Err] = JsonCodec[Err]
-}
+object Err extends ZioJsonCodec[Err]
+
 case class NotFound(message: String) extends Err(message)
+object NotFound                      extends ZioJsonCodec[NotFound]
